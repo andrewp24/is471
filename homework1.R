05c77457a1ef47f2c15390bca92721ae62d18b82
Andrew Peterson <andrewp2@umbc.edu>
2018-02-14 16:17:49 -0500

    first commit

---
+++ homework1.R
@@ -0,0 +1,92 @@
+########## Question 1 ##########
+
+#a
+l <- utf8ToInt('S')
+key <- 5
+new_l <- l-key
+modded <- mod(new_l, 128)
+q1a <- intToUtf8(modded) # answer is N
+
+#b
+l <- utf8ToInt('Y')
+key <- 2
+new_l <- l-key
+modded <- mod(new_l, 128)
+q1b <- intToUtf8(modded) # answer is w
+
+#c
+l <- utf8ToInt('Y')
+key <- 12
+new_l <- l-key
+modded <- mod(new_l, 128)
+q1c <- intToUtf8(modded) #answer is M
+
+#d
+l <- utf8ToInt('C')
+key <- 4
+new_l <- l-key
+modded <- mod(new_l, 128)
+q1d <- intToUtf8(modded) #We get a '?'. You could substract 65 from the integar by mod 91 and that would fix this, but it is a hardcode fix.
+
+########## Question 2 ##########
+
+#a
+l<-utf8ToInt('C')
+key<-5
+unencrypt_int<- l-65-key
+modded<-mod(unencrypt_int,26)
+new_int<-modded+65
+q2a <-intToUtf8(new_int) #answer is X
+
+#b
+l<-utf8ToInt('A')
+key<-2
+unencrypt_int<- l-65-key
+modded<-mod(unencrypt_int,26)
+new_int<-modded+65
+q2b <-intToUtf8(new_int) #answer is Y
+
+#c
+l<-utf8ToInt('H')
+key<-12
+unencrypt_int<- l-65-key
+modded<-mod(unencrypt_int,26)
+new_int<-modded+65
+q2c <-intToUtf8(new_int) #answer is V
+
+########## Question 3 ##########
+
+#a
+l<-utf8ToInt('C')
+key<-9
+encrypt_int<- l-65+key
+modded<-mod(encrypt_int,26)
+new_int<-modded+65
+q3a <-intToUtf8(new_int) #answer is L
+
+#b
+l<-utf8ToInt('J')
+key<-10
+encrypt_int<- l-65+key
+modded<-mod(encrypt_int,26)
+new_int<-modded+65
+q3b <-intToUtf8(new_int) #answer is T
+
+#c
+l<-utf8ToInt('D')
+key<-19
+encrypt_int<- l-65+key
+modded<-mod(encrypt_int,26)
+new_int<-modded+65
+q3c <-intToUtf8(new_int) #answer is W
+
+#d
+l<-utf8ToInt('Z')
+key<-12
+encrypt_int<- l-65+key
+modded<-mod(encrypt_int,26)
+new_int<-modded+65
+q3d <-intToUtf8(new_int) #answer is L
+
+########## Question 4 ##########
+
