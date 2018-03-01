25a0e0546f574247db0edcb1db4982c5b6a7d047
Andrew Peterson <andrewp2@umbc.edu>
2018-03-01 18:54:47 -0500

    question 6, 7, and 8

--- homework3.R
+++ homework3.R
@@ -73,9 +73,127 @@ my_mode = function(v){
   m = names(max_idx)
   m
 }
-my_mode(c(x[9], y[11], z[11])) # answer: 1
+m = my_mode(c(x[9], y[11], z[11])) # answer: 1
 
 ##### Question 6 #####
+#if x9 is equal to my_mode which is 1, then it will step
 
-if
+#setting copies of the vectors
+x2 = x
+y2 = y
+z2 = z
 
+if (x[9]==m){
+  #x step
+  #xor the following x[14], x[17], x[18], x[19]
+  # xor the first two and then the last two, then xor the results of those two
+  xorx1=bitwXor(x[14], x[17])
+  xorx2=bitwXor(x[18], x[19])
+  xorx3=bitwXor(xorx1, xorx2)
+  #xor3 will be the new first value
+  
+  #should be able to make a function for these
+  i=1
+  for(elemx in x){
+    
+    if(i-1!=0){
+      x2[i] = x[i-1]
+    }
+    i=i+1
+  }
+  x2[1]=xorx3
+}
+
+if (y[11]==m){
+  #y steps
+  #xor the following: y[21], y[22]
+  xory=bitwXor(y[21], y[22])
+  #xory will be the new first value
+  
+  i=1
+  for(elemy in y){
+    
+    if(i-1!=0){
+      y2[i] = y[i-1]
+    }
+    i=i+1
+  }
+  y2[1]=xory
+}
+
+if (z[11]==m){
+  #z steps
+  #xor the following z[8], z[21], x[22], x[23]
+  # xor the first two and then the last two, then xor the results of those two
+  xorz1=bitwXor(z[8], z[21])
+  xorz2=bitwXor(z[22], z[23])
+  xorz3=bitwXor(xorz1, xorz2)
+  #xor3 will be the new first value
+  
+  i=1
+  for(elemz in z){
+    
+    if(i-1!=0){
+      z2[i] = z[i-1]
+    }
+    i=i+1
+  }
+  z2[1]=xorz3
+  
+}
+#printing the original x,y,z and new x2,y2,z2 side by side
+print(x) # 0 1 0 0 1 1 0 1 0 1 0 0 1 0 0 0 0 1 0
+print(x2) # 0 1 0 0 1 1 0 1 0 1 0 0 1 0 0 0 0 1 0 #care about this one
+print(y) # 0 1 0 0 1 0 1 0 0 0 1 0 0 0 0 1 1 1 0 0 1 0
+print(y2) # 1 0 1 0 0 1 0 1 0 0 0 1 0 0 0 0 1 1 1 0 0 1 #care about this one
+print(z) # 0 1 1 1 0 0 0 0 0 1 1 0 1 1 1 0 0 1 1 0 0 0 0
+print(z2) # 0 0 1 1 1 0 0 0 0 0 1 1 0 1 1 1 0 0 1 1 0 0 0 # care about this one
+
+
+##### Question 7 #####
+
+#last values of each vector will be xor'd
+xorlast1=bitwXor(x2[19], y2[22])
+keystreamBit=bitwXor(xorlast1, z2[23]) # answer is 1
+
+##### Question 8 #####
+#function for the above
+
+a51_funcion = function(seed, keyLength) {
+  
+  seedInt = utf8ToInt(seed)
+  binarySeed = binary(seedInt, mb=7)
+  strsplitBinarySeed = strsplit(binarySeed, "") #8 times chr [1:8]
+  unlistBinarySeed = unlist(strsplitBinaryseed) #chr [1:64]
+  binaryAsInt = as.integer(c(unlistBinarySeed)) #int [1:64]
+  
+  x = vector(mode="integer", length = 19) #int [1:19] 0 1 0 0 1 1 0 1 0 1 ...
+  for (i in 1:19){
+    x[i] = binaryAsInt[i]
+  }
+  
+  y = vector(mode="integer", length = 22) #int [1:22] 0 1 0 0 1 0 1 0 0 0 ...
+  for (i in 1:22){
+    y[i] = binaryAsInt[i+19]
+  }
+  
+  z = vector(mode="integer", length = 23) #int [1:23] 0 1 1 1 0 0 0 0 0 1 ...
+  for (i in 1:23){
+    z[i] = binaryAsInt[i+41]
+  }
+  
+  k = vector(mode="integer", length = keyLength)
+  
+  my_mode = function(v){
+    freq_count = table(v)
+    max_idx = which.max(freq_count)
+    m = names(max_idx)
+    m
+  }
+  m = my_mode(c(x[9], y[11], z[11]))
+  
+  
+  
+}
+
+a51_funcion("johnsons", 64)
