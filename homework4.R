9b17e8ed8bf51c5191a826754a8d46c1427721ac
Andrew Peterson <andrewp2@umbc.edu>
2018-03-07 22:43:04 -0500

    answered last couple questions

--- homework4.R
+++ homework4.R
@@ -1,4 +1,5 @@
 library(seqinr)
+library(compositions)
 
 ##### Question 1 #####
 
@@ -15,7 +16,6 @@ v = unlist(strsplit(s, "")) # "1" "1" "0" "0" "1" "1"
 
 ##### Question 4 #####
 
-#Question 4
 ev = vector(mode="character", length = 8)
 ev = append(v[1:6], v[3:4], after = 2) # # "1" "1" "0" "0" "0" "0" "1" "1"
 
@@ -32,3 +32,44 @@ expand = function(binaryVec){
 expand("123456") # "1" "2" "4" "3" "4" "3" "5" "6"
 
 ##### Question 6 #####
+numbers = c(5,2,1,6,3,4,7,0)
+s11=binary(numbers) # [1] "101" "010" "001" "110" "011" "100" "111" "000"
+
+numbers2 = c(1,4,6,2,0,7,5,3)
+s12=binary(numbers2) # [1] "001" "100" "110" "010" "000" "111" "101" "011"
+
+##### Question 7 #####
+b=unlist(strsplit("1101", ""))
+tempVal=b
+tempVal=c(tempVal[2:4])
+tempVal=paste(tempVal[1:3])
+tempVal=toString(c(tempVal[1], tempVal[2], tempVal[3]))
+tempVal=gsub(pattern = ", ", replacement = "", x = tempVal, fixed = TRUE)
+tempVal=strtoi(test, base=2)
+
+if (b[1]==0) {
+  tempRet= s11[tempVal]
+} else {
+  tempRet= s12[tempVal]
+}
+
+##### Question 8 #####
+
+s11_function = function(binary_val){
+  b=unlist(strsplit(binary_val, ""))
+  tempVal=b
+  tempVal=c(tempVal[2:4])
+  tempVal=paste(tempVal[1:3])
+  tempVal=toString(c(tempVal[1], tempVal[2], tempVal[3]))
+  tempVal=gsub(pattern = ", ", replacement = "", x = tempVal, fixed = TRUE)
+  tempVal=strtoi(tempVal, base=2)
+  
+  if (b[1]==0) {
+    tempRet= s11[tempVal]
+  } else {
+    tempRet= s12[tempVal]
+  }
+  
+  return(tempRet)
+}
+s11_function("1001")
